27efb87196316e0b4ea568035f90725cc7d8de73
AmritPerera(L) <amritperera@yahoo.com>
2019-03-05 21:08:51 -0600

    filter works, need to accept correct input now

--- static/js/app.js
+++ static/js/app.js
@@ -2,39 +2,6 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-function generate_table() {
-  // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
-  var tbl = document.getElementsByTagName("table")[0];
-  var tblbody = document.getElementsByTagName("tbody")[0];
-
-  // creating all cells
-  for (var i = 0; i < tableData.length; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
-
-    for (var j = 0; j < 2; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode("cell in row "+i+", column "+j);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tbl);
-  // sets the border attribute of tbl to 2;
-  tbl.setAttribute("border", "2");
-}
-
 
 var tbl = document.getElementsByTagName("table")[0];
 var tblbody = document.getElementsByTagName("tbody")[0];
@@ -57,17 +24,17 @@ tableData.forEach((record) => {
 
 });
 
+function generate_table() {
+}
 
 
+// Create a custom filtering function
+function filterDate(date) {
+  return date.datetime == '1/11/2010';
+}
 
-console.log("===== Loop over each object in an array =====")
-// Loop through array of objects then each object
-tableData.forEach((record) => {
-  console.log(record);
+// filter() uses the custom function as its argument
+var userDate = tableData.filter(filterDate);
 
-  // Get the entries for each object in the array
-  Object.entries(record).forEach(([key, value]) => {
-    // Log the key and value
-    console.log(`Key: ${key} and Value ${value}`);
-  });
-}); 
+// Test
+console.log(userDate);
